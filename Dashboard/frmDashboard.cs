7449da58031fb6f9130254be870665d4807707d8
ValenGamerXD <valentinodidio@hotmail.com>
2021-05-12 19:43:22 -0300

    Fixed datagridview

--- Dashboard/frmDashboard.cs
+++ Dashboard/frmDashboard.cs
@@ -15,7 +15,8 @@ namespace Dashboard
         List<VentasProductos> Carro = new List<VentasProductos>();
         List<Producto> Productos = new List<Producto>();
         List<Venta> Ventas = new List<Venta>();
-        
+        float total = 0;
+
         public frmDashboard()
         {
             InitializeComponent();
@@ -23,7 +24,6 @@ namespace Dashboard
 
         private void btnVenta_Click(object sender, EventArgs e)
         {
-            float total = 0;
             foreach (VentasProductos aCarro in Carro){
                 foreach (Producto aProductos in Productos) {
                     if (aCarro.Producto_id == aProductos.Id){
@@ -32,19 +32,41 @@ namespace Dashboard
                     }
                 }
             }
-            textTotal.Text = String.Format("Total: {0}",total);
+            total = 0;
+            textTotal.Text = String.Format("Total: {0}", total);
             Ventas.Add(new Venta(idactual,total,DateTime.Now,Carro));
             Carro.Clear();
             idactual++;
-            //
+            dataGridView1.Rows.Clear();
         }
 
         private void btnAgregar_Click(object sender, EventArgs e)
         {
-            for (int i = 1; i <= Int32.Parse(textBoxCantidad.Text); i++)
+            uint aux=0, cant;
+            try
             {
-                Carro.Add(new VentasProductos(idactual, idproducto)); //aca se van a guardar los productos del carro
+                cant = UInt32.Parse(textBoxCantidad.Text);
             }
+            catch
+            {
+                cant = 1;
+            }
+            for (int i = 1; i <= cant; i++)
+            {
+                idproducto = UInt32.Parse(textBoxProducto.Text);
+                foreach (Producto aProductos in Productos)
+                {
+                    if (idproducto == aProductos.Id)
+                    {
+                        total += aProductos.Precio;
+                        aux = 1;
+                        dataGridView1.Rows.Add(aProductos.Nombre, aProductos.Precio);
+                        Carro.Add(new VentasProductos(idactual, idproducto)); //aca se van a guardar los productos del carro
+                    }
+                }
+            }
+            if (aux != 1) MessageBox.Show("Producto no encontrado!");
+            textTotal.Text = String.Format("Total: {0}", total);
             textBoxProducto.Clear();
             textBoxCantidad.Clear();
         }
@@ -52,7 +74,8 @@ namespace Dashboard
         private void frmDashboard_Load(object sender, EventArgs e)
         {
             Productos.Add(new Producto(1,"Cocacola",500,100));
-            dataGridView1.DataSource = Carro.ConvertAll(x => new { Value = x });
+            dataGridView1.Columns.Add("producto","producto");
+            dataGridView1.Columns.Add("precio", "precio");
             dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
         }
 
