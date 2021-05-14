64452354ecd5d976b4afd7622d6bb534063e37c4
Valen <valentinodidio943@gmail.com>
2021-05-13 23:41:53 -0300

    Update SQLQuery1.sql

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,6 +1,5 @@
-﻿use base1;
 create table productos(
-	id int IDENTITY(1,1) PRIMARY KEY,
+    id int IDENTITY(1,1) PRIMARY KEY,
     nombre text NOT NULL,
     descripcion text,
     marca text,
@@ -8,13 +7,13 @@ create table productos(
     stock int NOT NULL DEFAULT 0,
 );
 create table ventas(
-	id int IDENTITY(1,1) PRIMARY KEY,
+    id int IDENTITY(1,1) PRIMARY KEY,
     monto float NOT NULL,
     fecha datetime NOT NULL,
-    productos int,
 );
 
 create table ventas_productos(
-	id int IDENTITY(1,1) PRIMARY KEY,
-    producto_id int,
+    id int IDENTITY(1,1) PRIMARY KEY,
+    venta_id int NOT NULL,
+    producto_id int NOT NULL,
 )
